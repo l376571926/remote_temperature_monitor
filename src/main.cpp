0b61f54e3aeea6e4510701d2fc5bba882bd6cd31
李艺为 <liyiwei18@h-partners.com>
2023-08-09 09:02:53 +0800

    连接前打印扫描到的附近的wifi
    Signed-off-by: 李艺为 <liyiwei18@h-partners.com>

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ const char *mqtt_server = "183.230.40.39";
 
 ESP8266WiFiMulti wifiMulti;
 // WiFi connect timeout per AP. Increase when connecting takes longer.
-const uint32_t connectTimeoutMs = 2000;
+const uint32_t connectTimeoutMs = 5000;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -31,13 +31,72 @@ char msg[MSG_BUFFER_SIZE];
 
 DHT dht(DHTPIN, DHTTYPE);
 
+void print_scan_result_info() {
+    String ssid;
+    int32_t rssi;
+    uint8_t encryptionType;
+    uint8_t *bssid;
+    int32_t channel;
+    bool hidden;
+    int scanResult;
+
+    Serial.println(F("Starting WiFi scan..."));
+
+    scanResult = WiFi.scanNetworks(/*async=*/false, /*hidden=*/true);
+
+    if (scanResult == 0) {
+        Serial.println(F("No networks found"));
+    } else if (scanResult > 0) {
+        Serial.printf(PSTR("%d networks found:\n"), scanResult);
+
+        // Print unsorted scan results
+        for (int8_t i = 0; i < scanResult; i++) {
+            WiFi.getNetworkInfo(i, ssid, encryptionType, rssi, bssid, channel, hidden);
+
+            // get extra info
+            const bss_info *bssInfo = WiFi.getScanInfoByIndex(i);
+            String phyMode;
+            const char *wps = "";
+            if (bssInfo) {
+                phyMode.reserve(12);
+                phyMode = F("802.11");
+                String slash;
+                if (bssInfo->phy_11b) {
+                    phyMode += 'b';
+                    slash = '/';
+                }
+                if (bssInfo->phy_11g) {
+                    phyMode += slash + 'g';
+                    slash = '/';
+                }
+                if (bssInfo->phy_11n) {
+                    phyMode += slash + 'n';
+                }
+                if (bssInfo->wps) {
+                    wps = PSTR("WPS");
+                }
+            }
+            Serial.printf(PSTR("  %02d: [CH %02d] [%02X:%02X:%02X:%02X:%02X:%02X] %ddBm %c %c %-11s %3S %s\n"), i,
+                          channel, bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5], rssi,
+                          (encryptionType == ENC_TYPE_NONE) ? ' ' : '*', hidden ? 'H' : 'V', phyMode.c_str(), wps,
+                          ssid.c_str());
+            yield();
+        }
+    } else {
+        Serial.printf(PSTR("WiFi scan error %d"), scanResult);
+    }
+}
+
 void setup_wifi() {
     // Don't save WiFi configuration in flash - optional
     WiFi.persistent(false);
 
     WiFi.mode(WIFI_STA);
 
+    print_scan_result_info();
+
     // Register multi WiFi networks
+    wifiMulti.addAP("荣耀8A", "88888888");
     wifiMulti.addAP("liyiwei_test", "88888888");
     wifiMulti.addAP("HUAWEI P30 Pro", "88888888");
     wifiMulti.addAP("HUAWEI_57E0", "88888888");
@@ -47,7 +106,7 @@ void setup_wifi() {
     // Maintain WiFi connection
     if (wifiMulti.run(connectTimeoutMs) == WL_CONNECTED) {
         Serial.print("WiFi connected: ");
-        Serial.print(WiFi.SSID());
+        Serial.println(WiFi.SSID());
         Serial.print(" IP address: ");
         Serial.println(WiFi.localIP());
     } else {
@@ -83,6 +142,7 @@ void reconnect() {
 }
 
 void setup() {
+    delay(5000);
     // write your initialization code here
     Serial.begin(115200);
     setup_wifi();
